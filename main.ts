7d04e72c99dc913f24a9f163001923c0c35b0aa1
Mosesmunyasya <mosesmunyasya@gmail.com>
2024-01-18 16:27:57 +0300

    first commit

---
+++ main.ts
@@ -0,0 +1,18 @@
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number [] = [1,2,3];
+let f: any [] [1, true,'a',false];
+
+
+let a: Number;
+a = 1;
+a = true;
+const ColorRed = 0;
+const ColorGreen = 1;
+const ColorBlue = 2;
+const ColorPruple = 3;
+
+enum Color { let Red = 0, Green = 1, Blue = 2, Purple = 3};
+let backgroundColor =Color.Red;
